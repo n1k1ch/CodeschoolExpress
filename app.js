e110f1a779889515c9880f02af5278554e63d8fb
Nikita Cherevkov <nikita.cherevkov@gmail.com>
2015-04-08 10:46:27 -0400

    level 3

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var app = express();
 
+var blocks = {
+	'Fixed':'Fastened securely in position',
+	'Movable':'Capable of being moved',
+	'Rotating':'Moving in a circle around its center'
+};
+
+var locations = {
+	'Fixed': 'First floor', 'Movable': 'Elevator', 'Rotating': 'Penthouse'
+};
+
 var logger = require('./logger');
 app.use(logger);
+
+
+app.param('name', function(request, response, next){
+	var name = request.params.name;
+	var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+	request.blockName = block;
+	next();
+});
 
 
 /*
@@ -18,7 +36,35 @@ app.get('/blocks', function(request, response) {
 	//3) response.json(blocks);
 	//4) response.redirect('/parts');
 	//7) response.redirect(301, '/parts');
-	response.send(blocks);
+
+	if(request.query.limit >= 0) {
+		response.json(blocks.slice(0, request.query.limit));
+	} else {
+		response.send(blocks);
+	}
+});
+
+app.get('/blocks/:name', function(request, response){
+	
+	var description = blocks[request.blockName];
+
+	if(!description) {
+		response.status(404)
+		.json('No description found for ' + request.params.name);
+	} else {
+		response.json(description);
+	}
+});
+
+app.get('/locations/:name', function(request, response){
+	var location = locations[request.blockName];
+
+	if(!location) {
+		response.status(404)
+		.json('No location found for ' + request.params.name);
+	} else {
+		response.json(location);
+	}
 });
 
 
